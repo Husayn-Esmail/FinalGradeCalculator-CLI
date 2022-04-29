35b9fa66f48e4592d2c6565a0d29d2d447aa311f
Husayn Esmail <31267171+Husayn-Esmail@users.noreply.github.com>
2022-04-29 13:33:18 -0600

    tried to transcribe the C to python, my brain is getting confused I'm tired. Also, I realized how confusing my input prompts are and have left a comment to fix it

--- Python/FGC.py
+++ Python/FGC.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 # the above line makes this python program executable (assuming you chmod +x)
+
+def main():
+    numGrades = input("Enter the number of grades you will be inputting: %d")
+    grades = []
+    for grade in range(0,numGrades):
+        # on second thought this clarification of input is really confusing.
+        # need to fix this. 
+        user_grade = input("Enter grade (integer): %d")
+        grades.append(user_grade)
+        weight = input("Enter weight (integer): %d")
+
